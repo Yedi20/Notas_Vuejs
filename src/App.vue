57a924cd09a61df5aafc0affe53859c4839a96a2
yedi20 <20185857@itla.edu.do>
2022-05-27 21:51:37 -0400

    v1 sin back end

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Tarea />
+</template>
+
+<script>
+import Tarea from './components/Tarea'
+export default {
+  name: 'App',
+  components: {
+    Tarea
+    
+  }
+  
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Tarea.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+  <h1 class="display-4 text-center">Listado de Notas</h1>
+  <hr>
+  <div class="row">
+      <div class="col-lg-8 offset-lg-2">
+          <div class="card mt-4">
+              <div class="card-body">
+                  <div class="input-group">
+                      <input type="text"  v-model="tarea"
+                      class="form-contro form-control-lg" placeholder="Agregar notas">
+                      <div class="input-group-append">
+                          <button v-on:click="agregarTarea()"
+                          class="btn btn-success btn-lg">Agregar</button>
+                      </div>
+                  </div>
+                  <br>
+                  <h5 v-if="listTarea.length == 0">No hay Notas a Realizar</h5>
+                <!-- {{listTarea}}  -->
+                  <ul class="list-group">
+                      <li v-for="(tarea, index ) of listTarea" :key="index"
+                      class="list-group-item  d-flex justify-content-between">
+                          <span class="cursor" v-bind:class="{'text-success': tarea.estado}"
+                          v-on:click="editarTarea(tarea, index)">
+                              <i  v-bind:class="[tarea.estado ? 'fas fa-check-circle' : 'far fa-circle']" ></i>
+
+                          </span>
+                          {{tarea.nombre}}
+                          <!-- Hacer la tarea -->
+
+                          <span class="text-danger cursor" v-on:click="eliminarTarea(index)">
+                              <i class="fas fa-trash-alt"></i>
+                          </span>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </div>
+    </div>
+</template>
+
+<script>
+    export default {
+          name: 'TareaA',
+         data(){
+             return {
+                 tarea: '',
+                 listTarea: []
+             }
+
+         },
+         methods:{
+             agregarTarea(){
+                 const tarea = {
+                     nombre: this.tarea,
+                     estado: false
+                 }
+                 this.listTarea.push(tarea);
+                 this.tarea ='';
+             },
+             eliminarTarea(index){
+                 this.listTarea.splice(index, 1)
+                 
+             },
+             editarTarea(tarea, index){
+                 this.listTarea[index].estado = !tarea.estado
+
+             }
+
+         }
+    }
+</script>
+
+<style scoped>
+.cursor{
+    cursor: pointer;
+}
+</style>
